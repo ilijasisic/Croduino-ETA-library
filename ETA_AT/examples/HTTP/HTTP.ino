b86563c93e45a7fcc03e7c614b09da4969507aaa
matejvukovic <47967863+matejvukovic@users.noreply.github.com>
2019-02-25 10:24:25 +0100

    Update HTTP.ino

--- ETA_AT/examples/HTTP/HTTP.ino
+++ ETA_AT/examples/HTTP/HTTP.ino
@@ -1,3 +1,6 @@
+//This example shows how to make HTTP request
+//HTTPS you can use are (GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, CONNECT, PATCH)
+//In this example we will use GET metod
 #include <ETA_AT.h>
 
 unsigned long bauds = 19200; //You should use baud rate of 19200
@@ -5,23 +8,29 @@ const char apn[] PROGMEM = {"internet.ht.hr"}; //APN name
 const char server[] PROGMEM = {"thingspeak.com"};//Server address you are connecting
 const char api[] PROGMEM = {"https://api.thingspeak.com/apps/thinghttp/send_request?api_key=LZQ1MEOQP6XDDW59"};//API link, data you want to GET
 const char host[] PROGMEM = {"www.ferit.unios.hr"};// Host address
-String message = ""; //SMSread returns String so we can save it here and use it later
+String message = ""; //Returns String with HTTP info so we can save it here and use it later
+String data; //Data you want to read from HTTP message
+
 ETA_AT SIM; //Create an ETA_AT object
 
 void setup() {
   Serial.begin(bauds); //Setting baud rate
-  while (!Serial) {
+  while (!Serial)
+  {
     ; //Waiting until the baud rate is set
   }
   SIM.begin(bauds); //Setting baud rate
   delay(100);
 
-  SIM.powerOn(); //Turning on the A6 module
-  SIM.Test(); //Establishing communication with the module
-message=SIM.requestHTTP(apn, server, api, host);
-Serial.println("pocetak");
-Serial.println(message);
-Serial.println("pocetak2");
+  SIM.powerOn();  //MUST HAVE!!Turning on the A6 module
+  SIM.Test();     //MUST HAVE!! Starts commuication with A6_module(OK: +CIEV: READY)
+  message = SIM.requestHTTP(apn, server, api, host);//Returns String with HTTP information
+  Serial.println(message);//Print HTTP request
+ 
+  //Example how you can get data from HTTP request
+  delay(4000);
+  data = message.substring(0, message.indexOf(13));//Saving data from beginig of message until first <CR>
+  Serial.println(data);//Print data you saved
 
 }
 
